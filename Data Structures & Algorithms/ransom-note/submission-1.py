8331d441fea40f69d0252d9be7841ef3d786be34
Ansh Chopra <anshchopra1512@gmail.com>
2026-08-18 23:42:15 +0530

    Refactor canConstruct method in submission-1.py

--- Data Structures & Algorithms/ransom-note/submission-1.py
+++ Data Structures & Algorithms/ransom-note/submission-1.py
@@ -3,11 +3,9 @@ class Solution:
 
         freq = {}
 
-        # Magazine ke characters count karo
         for ch in magazine:
             freq[ch] = freq.get(ch, 0) + 1
 
-        # RansomNote ke har character ko check karo
         for ch in ransomNote:
             if ch not in freq or freq[ch] == 0:
                 return False
